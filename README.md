80cef6485a7b0420a0692bc7a47eaa3eb7bfc2b3
Jabari Bell <jabari.bell@pxlflu.net>
2014-01-07 23:47:52 -0500

    jasmine setup with test spec

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 scarf-api
 =========
+
+#Requirements
+* Jasmine node: npm install jasmine-node -g
---
+++ spec/myFirstSpec.js
@@ -0,0 +1,5 @@
+describe("A suite", function() {
+	it("contains some lolz", function() {
+		expect(true).toBe(false);
+	});
+});
